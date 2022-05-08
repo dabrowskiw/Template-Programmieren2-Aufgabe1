edf7c2b5e0419f3cf27c92648509cfaad7743921
Katrin Lang <mail@katrinlang.de>
2022-05-08 14:25:15 +0200

    Predefined functions as in Cultur assignment

--- src/main/org/htw/prog2/aufgabe1/SeqFile.java
+++ src/main/org/htw/prog2/aufgabe1/SeqFile.java
@@ -10,6 +10,25 @@ public class SeqFile {
      */
     public SeqFile(String filename) {
     }
+    
+    /**
+     * Reads the specified FASTA file.
+     * @param filename The path to the FASTA file
+     * @return false if the file could not be parsed (wrong format, does not exist), true otherwise.
+     */
+    private boolean readFile(String filename) {
+        return false;
+    }
+
+    /**
+     * Adds the sequence in the passed StringBuilder to the internal list and also sets the first sequence if it
+     * is still empty.
+     * @param seq SequenceBuilder to get the sequence from.
+     * @return The length of the added sequence.
+     */
+    private int addSequence(StringBuilder seq) {
+        return -1;
+    }
 
     /**
      *
